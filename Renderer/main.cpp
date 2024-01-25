bf8d5cc06ed6d563a8be3fde803473053c91b187
kancall <3031806889@qq.com>
2024-01-25 22:02:12 +0800

    切线空间，使用的是未正交化的算式

--- Renderer/main.cpp
+++ Renderer/main.cpp
@@ -7,7 +7,7 @@
 #include "our_gl.h"
 using namespace std;
 
-Vec3f camera(-1, 0, 5);
+Vec3f camera(1, 0, 5);
 Vec3f center(0, 0, 0);
 Vec3f up(0, 1, 0);
 Vec3f lightDir = Vec3f(1, 1, 1).normalize();
@@ -52,7 +52,7 @@ struct Shader :public IShader
 
 		return Viewport * Projection * ModelView * v;
 	}
-	virtual bool fragment(Vec3f bc, TGAColor& color) 
+	virtual bool fragment(Vec3f bc, TGAColor& color)
 	{
 		Vec2f uv = vertex_uv * bc;
 		Vec3f normal = proj<3>(uniform_MIT * embed<4>(model->normal(uv))).normalize();
@@ -72,6 +72,8 @@ struct Shader :public IShader
 
 struct TangentShader :IShader
 {
+	//注意，mat<i,t,T>是vector[t][i]
+	mat<3, 3, float> vertexs; //三角形三个顶点
 	mat<2, 3, float> vertex_uv;
 	mat<3, 3, float> vertex_nm; //存储三个顶点的法线
 
@@ -79,21 +81,40 @@ struct TangentShader :IShader
 	virtual Vec4f vertex(int iface, int nvert)
 	{
 		vertex_uv.set_col(nvert, model->uv(iface, nvert));
-		vertex_nm.set_col(nvert, proj<3>(embed<4>(model->norm(iface, nvert))));
-		Vec4f v = embed<4>(model->vert(iface, nvert));
-
-		return Viewport * Projection * ModelView * v;
+		vertex_nm.set_col(nvert, proj<3>(uniform_MIT * embed<4>(model->norm(iface, nvert))));
+		Vec4f point = Viewport * Projection * ModelView * embed<4>(model->vert(iface, nvert));
+		vertexs.set_col(nvert, proj<3>(point / point[3]));
+		return  point;
 	}
 
 	virtual bool fragment(Vec3f bc, TGAColor& color)
 	{
 		Vec2f uv = vertex_uv * bc;
-		Vec3f normal = (vertex_nm * bc).normalize();
+		Vec3f bn = (vertex_nm * bc).normalize();
 
-		//通过TBN矩阵将法线变换到世界空间中去
+		//通过TBN矩阵将法线变换到世界空间中去，这里用的矩阵其实是笔记中那个有问题的算式，可以优化一下
+		Vec3f e1 = vertexs.col(1) - vertexs.col(0); //point1-point0
+		Vec3f e2 = vertexs.col(2) - vertexs.col(0); //point2-point0
+		Vec3f xy1 = Vec3f(vertex_uv[0][1] - vertex_uv[0][0], vertex_uv[0][2] - vertex_uv[0][0], 0); //vec3f(u1-u0,u2-u0,0)
+		Vec3f xy2 = Vec3f(vertex_uv[1][1] - vertex_uv[1][0], vertex_uv[1][2] - vertex_uv[1][0], 0); //vec3f(v1-v0,v2-v0,0)
 
+		mat<3, 3, float> A;
+		A[0] = e1;
+		A[1] = e2;
+		A[2] = bn;
+		A = A.invert();
 
-		float diff = max(dot(normal, lightDir), 0.f);
+		Vec3f t = A * xy1;
+		Vec3f b = A * xy2;
+
+		mat<3, 3, float> TBN;
+		TBN.set_col(0, t.normalize());
+		TBN.set_col(1, b.normalize());
+		TBN.set_col(2, bn);
+
+		Vec3f n = (TBN * model->normal(uv)).normalize();
+
+		float diff = max(dot(n, lightDir), 0.f);
 		color = model->diffuse(uv) * diff;
 
 		return false;
@@ -122,7 +143,7 @@ int main()
 		Vec3i points[3];
 		for (int i = 0; i < 3; i++)
 		{
-			for (int t = 0; t < 3; t++) 
+			for (int t = 0; t < 3; t++)
 				points[t][i] = screen[t][i];
 		}
 		triangle(points, shader, zbuffer, image);
